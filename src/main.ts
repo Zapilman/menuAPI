61669d787a6f3f5d380c654922de9a8f6142e46d
Zapilman <17uraz12@gmail.com>
2023-05-09 09:16:42 +0300

    update env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.enableCors();
